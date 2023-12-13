a5526054ab518eac8adf4606d21ff4f125eaf304
Ferenc Hechler <ferenc.hechler@telekom.de>
2023-12-13 19:26:06 +0100

    create feature branch fxyz-test.

---
+++ src/main/java/org/openjfx/Sample.java
@@ -0,0 +1,49 @@
+package org.openjfx;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.CullFace;
+import javafx.stage.Stage;
+import org.fxyz3d.shapes.primitives.SpringMesh;
+import org.fxyz3d.utils.CameraTransformer;
+
+public class Sample extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setNearClip(0.1);
+        camera.setFarClip(10000.0);
+        camera.setTranslateX(10);
+        camera.setTranslateZ(-100);
+        camera.setFieldOfView(20);
+
+        CameraTransformer cameraTransform = new CameraTransformer();
+        cameraTransform.getChildren().add(camera);
+        cameraTransform.ry.setAngle(-30.0);
+        cameraTransform.rx.setAngle(-15.0);
+
+        SpringMesh spring = new SpringMesh(10, 2, 2, 8 * 2 * Math.PI, 200, 100, 0, 0);
+        spring.setCullFace(CullFace.NONE);
+        spring.setTextureModeVertices3D(1530, p -> p.f);
+
+        Group group = new Group(cameraTransform, spring);
+
+        Scene scene = new Scene(group, 600, 400, true, SceneAntialiasing.BALANCED);
+        scene.setFill(Color.BISQUE);
+        scene.setCamera(camera);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("FXyz3D Sample");
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
